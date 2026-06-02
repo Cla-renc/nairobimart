01e526c5a8a80c52bcdc0b7c3abfc92325bf9bf7
Clarence <yaaclarence@gmail.com>
2026-06-02 18:14:38 +0300

    Allow Google to link to existing email accounts

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            allowDangerousEmailAccountLinking: true,
         })
     );
 }
