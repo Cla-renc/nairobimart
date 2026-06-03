7b1468c8e0cdff2c8909acaa9b4579899f195eae
Clarence <yaaclarence@gmail.com>
2026-06-03 12:31:05 +0300

    fix: add Google authorization params to resolve PKCE Invalid code verifier error

--- auth.config.ts
+++ auth.config.ts
@@ -8,6 +8,13 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             allowDangerousEmailAccountLinking: true,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                },
+            },
         })
     );
 }
